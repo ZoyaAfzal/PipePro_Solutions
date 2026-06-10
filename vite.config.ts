20f1facc7c569b36b5d15c15cf8c161a9dfed18d
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2026-06-10 13:27:26 -0700

    PipePro_Solutions

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
-import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
-    tsconfigPaths(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    tsconfigPaths: true,
   },
   build: {
     outDir: "dist",
